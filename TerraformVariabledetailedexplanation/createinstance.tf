0fd96236d8e5114197544ff937b56d3cd13391bd
Harish-365 <bvpetro97@gmail.com>
2025-05-01 20:56:23 +0530

    addnew

--- TerraformVariabledetailedexplanation/createinstance.tf
+++ TerraformVariabledetailedexplanation/createinstance.tf
@@ -1,4 +1,6 @@
 resource "aws_instance" "Myfirstinstance" {
   ami = "ami-00045d6bafc77e3dc"
   instance_type = "t2.micro"
+
+  security_groups = "${var.Security_Groups}"
 }
--- TerraformVariabledetailedexplanation/variables.tf
+++ TerraformVariabledetailedexplanation/variables.tf
@@ -2,3 +2,8 @@ variable "AWS_REGION" {
     default = "us-east-1"
   
 }
+
+variable "Security_Groups" {
+    type = list
+    default = ["sg-123", "sg-1234"]
+}
