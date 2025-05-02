2f2081c0c24d50aaae87552fb5da564ac3ac6b5d
Harish-365 <bvpetro97@gmail.com>
2025-05-02 21:31:08 +0530

    add21

--- TerraformVariabledetailedexplanation/Terraform concepts building blocks/DemoData source/variables.tf
+++ TerraformVariabledetailedexplanation/Terraform concepts building blocks/DemoData source/variables.tf
@@ -5,7 +5,7 @@ variable "AWS_REGION" {
 
 variable "AMIS" {
     type = map
-    default = {us-east-1 = "ami-0f40c8f97004632f9"
+    default = {us-east-1 = "ami-00076e19db6f2c629"
                us-east-2 = "ami-05692172625678b4e"
 
 }
