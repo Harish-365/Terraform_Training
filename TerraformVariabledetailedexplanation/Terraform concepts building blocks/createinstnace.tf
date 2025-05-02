af17f5e9b54ca260ff2a44ad0e0d882c84501943
Harish-365 <bvpetro97@gmail.com>
2025-05-02 11:32:36 +0530

    add4

--- TerraformVariabledetailedexplanation/Terraform concepts building blocks/createinstnace.tf
+++ TerraformVariabledetailedexplanation/Terraform concepts building blocks/createinstnace.tf
@@ -1,6 +1,30 @@
+resource "aws_key_pair" "levelup_key" {
+  key_name = levelup_key
+  public_key = file(var.AWS_PUBLICKEY)
+}
+
 resource "aws_instance" "Myfirstinstance" {
   ami = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
+  key_name      = aws_key_pair.levelup_key.key_name
+}
 
-  security_groups = "${var.Security_Groups}"
+provisioner "file"{
+  source = "INstalllNginix.sh"
+  destination = "/tmp/INstalllNginix.sh"
+}
+
+provisioner "remote-exec"{
+  inline = [
+    "chmod +x /tmp/INstalllNginix.sh"
+    "sudo sed -i -e 's/\r$//' /tmp/INstalllNginix.sh"
+    "sudo /tmp/INstalllNginix.sh"
+  ]
+}
+
+connection {
+  host = coalesce(self.public_ip, self.private_ip)
+  type = ssh
+  user = var.INSTANCE_USERNAME
+  private_key = file(var.AWS_PRIVATEKEY)
 }
--- TerraformVariabledetailedexplanation/Terraform concepts building blocks/variables.tf
+++ TerraformVariabledetailedexplanation/Terraform concepts building blocks/variables.tf
@@ -14,3 +14,15 @@ variable "AMIS" {
                us-east-2 = "ami-05803413c51f242b7"
 }
 }
+
+variable "AWS_PUBLICKEY" {
+  default = "levelup_key.pub"
+}
+
+variable "AWS_PRIVATEKEY" {
+  default = "levelup_key"
+}
+
+variable "INSTANCE_USERNAME" {
+  default = "ubuntu"
+}
