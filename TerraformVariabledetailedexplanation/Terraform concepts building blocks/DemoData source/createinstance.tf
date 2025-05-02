965136cc09855a086e5389710d73475417b92f29
Harish-365 <bvpetro97@gmail.com>
2025-05-02 15:29:36 +0530

    add10

--- TerraformVariabledetailedexplanation/Terraform concepts building blocks/DemoData source/createinstance.tf
+++ TerraformVariabledetailedexplanation/Terraform concepts building blocks/DemoData source/createinstance.tf
@@ -1,4 +1,7 @@
+data "aws_availability_zones" "available" {}
+
 resource "aws_instance" "Myfirstinstance" {
   ami = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
+  availability_zone = data.aws_availability_zones.available.names[1]
 }
