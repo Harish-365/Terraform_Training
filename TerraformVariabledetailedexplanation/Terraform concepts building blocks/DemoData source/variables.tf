2ffa79be536ef132561f3d452c992c50f6e6a155
Harish-365 <bvpetro97@gmail.com>
2025-05-02 16:04:58 +0530

    add12

--- TerraformVariabledetailedexplanation/Terraform concepts building blocks/DemoData source/variables.tf
+++ TerraformVariabledetailedexplanation/Terraform concepts building blocks/DemoData source/variables.tf
@@ -10,7 +10,7 @@ variable "Security_Groups" {
 
 variable "AMIS" {
     type = map
-    default = {us-east-1 = "ami-00045d6bafc77e3dc"
-               us-east-2 = "ami-05803413c51f242b7"
+    default = {us-west-1 = "ami-00045d6bafc77e3dc"
+               us-west-2 = "ami-05803413c51f242b7"
 }
 }
