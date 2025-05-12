dee3f188f130d54f0091f394a893421c1e0feada
Harish-365 <bvpetro97@gmail.com>
2025-05-12 22:58:28 +0530

    add39

--- TerraformVariabledetailedexplanation/Terraform for AWS Cloud/security_group.tf
+++ TerraformVariabledetailedexplanation/Terraform for AWS Cloud/security_group.tf
@@ -4,7 +4,7 @@ resource "aws_security_group" "levelup-security" {
     ingress { ## Inbound traffic
         from_port = "22" ##allowing ssh port so that we can connect using ssh to the remote VM
         to_port = "22" ##  This allows SSH (port 22) from anywhere (0.0.0.0/0). Since both ports are the same, it allows only port 22.
-        protocol = "ssh"
+        protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"] ## here the cidr block is mentioned with in the [] which means it is a list and it allows any kind of ip.
     }
 
