e520ef5045cc3a493135b40b9b7d97bd0d8ff9dd
Harish-365 <bvpetro97@gmail.com>
2025-05-12 19:36:49 +0530

    add30

---
+++ TerraformVariabledetailedexplanation/Terraform for AWS Cloud/variables.tf
@@ -0,0 +1,16 @@
+variable "AWS_REGION" {
+    default = "us-east-1"
+  
+}
+
+variable "Security_Groups" {
+    type = list
+    default = ["sg-123", "sg-1234"]
+}
+
+variable "AMIS" {
+    type = map
+    default = {us-east-1 = "ami-00045d6bafc77e3dc"
+               us-east-2 = "ami-05803413c51f242b7"
+}
+}
