1a3b303ee19b30a4a0d4709d5130afa4321bfc24
Harish-365 <bvpetro97@gmail.com>
2025-05-12 22:50:43 +0530

    add37

--- TerraformVariabledetailedexplanation/Terraform for AWS Cloud/createinstance.tf
+++ TerraformVariabledetailedexplanation/Terraform for AWS Cloud/createinstance.tf
@@ -1,10 +1,10 @@
 resource "aws_key_pair" "levelup_key" {
     key_name = "levelup_key"
-    public_key = file(var.AWS_PUBLICKEY) ## create key uisng ssh -keygen -f levelup_key
+    public_key = file(var.AWS_PUBLICKEY) ## create key uisng ssh-keygen -f levelup_key
 }
 
 resource "aws_instance" "Myfirstinstance" {
-    ami = lookup(var.AMIS.var.AWS_REGION)
+    ami = lookup(var.AMIS, var.AWS_REGION)
     vpc_security_group_ids = aws_security_group.levelup-security
     instance_type = "t2.micro"
     key_name      = aws_key_pair.levelup_key.key_name 
