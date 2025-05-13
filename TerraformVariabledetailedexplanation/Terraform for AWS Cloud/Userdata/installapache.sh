ac22c38a563c4ff9dd67a079b6b7bb59b1fa2355
Harish-365 <bvpetro97@gmail.com>
2025-05-13 12:29:59 +0530

    add42

--- TerraformVariabledetailedexplanation/Terraform for AWS Cloud/Userdata/installapache.sh
+++ TerraformVariabledetailedexplanation/Terraform for AWS Cloud/Userdata/installapache.sh
@@ -3,4 +3,4 @@ apt-get update
 apt-get install -y apache2
 systemctl start apache2
 systemctl enable apache2
-echo "<h1>Deployed Via Terraform<h1>" | sudo tee /var/www/html/index.html
+echo "<h1>Deployed Via Terraform<h1>" | sudo tee /var/www/html/index.html  ##<h1> it is heading no.1 in html which means a large size heading
