c2304793bcee50e6013a71abb1171d63906b0b3a
Harish-365 <bvpetro97@gmail.com>
2025-05-19 09:46:06 +0530

    add75

---
+++ TerraformVariabledetailedexplanation/Terraform for AWS Cloud II/AWS_ELB/autoscaling.tf
@@ -0,0 +1,62 @@
+##public key
+
+resource "aws_key_pair" "levelup_key" {
+    key_name = "levelup_key"
+    public_key = file(var.AWS_PUBLICKEY)
+  
+}
+
+# the name_prefix argument in Terraform is used to set a prefix for resource names, and AWS automatically appends a random suffix to make each name unique.
+# 🔹 How name_prefix Works
+# - Instead of specifying an exact name (name), Terraform allows AWS to generate unique names by appending random characters.
+# - Useful when you don’t need an exact name but require distinct resource identifiers. 
+# - Terraform might create my-launch-config-abc123 or my-launch-config-xyz789, ensuring uniqueness.
+# 🔹 Why Use name_prefix?
+# ✅ Prevents name conflicts.
+# ✅ Ideal for auto-generated names in Auto Scaling Groups, S3 buckets, and EC2 instances.
+# ✅ Helps when scaling multiple resources dynamically.
+
+##Launch configuration:
+resource "aws_launch_template" "levelup-launch-template" {
+  name_prefix   = "levelup-launch-template"
+  image_id      = lookup(var.AMIS, var.AWS_REGION)
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.levelup_key.key_name
+  vpc_security_group_ids = [aws_security_group.levelup-security-instance]
+  user_data = "#!/bin/bash\napt-get update\napt-get -y install net-tools nginx\nMYIP=`ifconfig | grep -E '(inet 10)|(addr:10)' | awk '{ print $2 }' | cut -d ':' -f2`\necho 'Hello Team\nThis is my IP: '$MYIP > /var/www/html/index.html"
+
+  lifecycle {
+    create_before_destroy = true
+
+  }
+}
+
+## Auto scaling group:
+
+resource "aws_autoscaling_group" "levelup-autoscaling" {
+    name = "levelup-autoscaling"
+    vpc_zone_identifier = [aws_subnet.levelupvpc-public-1, aws_subnet.levelupvpc-public-2]
+launch_template {
+    id      = aws_launch_template.levelup-launch-template.id
+    version = "$Latest"
+  }
+
+    min_size = 2 ##minimum instance should be there
+    max_size = 2
+    health_check_grace_period = 200 ## after 200s of the continous instance failure then it will create a new instance
+    health_check_type = "ELB"
+    load_balancers = [aws_elb.levelup-elb.name]
+    force_delete = true  ## in Auto Scaling to ensure resources are deleted, even if they are still running.
+
+    tag {
+        key = "Name"
+        value = "Levelup Custom Ec2 instance for ELB"
+        propagate_at_launch = true ## ensures that tags applied to the Auto Scaling Group (ASG) are automatically assigned to newly launched instances.
+
+    }
+}
+
+output "ELB" {
+  value = aws_elb.levelup-elb.dns_names
+  
+}
---
+++ TerraformVariabledetailedexplanation/Terraform for AWS Cloud II/AWS_ELB/elb.tf
@@ -0,0 +1,93 @@
+##AWS ELB configuration:
+
+resource "aws_elb" "levelup-elb" {
+    name = "levelup-elb"
+    subnets = [aws_subnet.levelupvpc-public-1, aws_subnet.levelupvpc-public-2]
+    security_groups = [aws_security_group.levelup-securityelb.id]
+    listener {
+      instance_port = 80 ##"our ec2 instance"
+      instance_protocol = "http"
+      lb_port = 80
+      lb_protocol = "http"
+    }
+    health_check {
+      healthy_threshold = 2  ## - The instance must pass 2 consecutive health checks to be considered healthy.
+      unhealthy_threshold = 2 ## - The instance must fail 2 consecutive health checks before being marked unhealthy.
+      timeout = 3 ## -  If an instance doesn’t respond within 3 seconds, it’s considered a failed check.
+      target = "HTTP:80/" ## - The load balancer checks the root URL (/) on port 80 using HTTP to verify if the instance is healthy.
+      interval = 30 ## - Health checks happen every 30 seconds.
+    }
+
+    cross_zone_load_balancing = true ## - I- This ensures that the load balancer distributes traffic evenly across all availability zones.
+    connection_draining = true ## If an instance is marked for removal (or becomes unhealthy), this setting allows active connections to finish before the instance is detached.
+    connection_draining_timeout = 400 ##- Defines how long the load balancer should wait for existing connections to complete before shutting down the instance.
+                                      ##- In this case, the load balancer will wait 400 seconds before terminating an instance that is draining connections. 
+
+
+    tags = {
+      Name = "levelup-elb"
+    }
+
+    
+}
+
+## security group for elb
+
+resource "aws_security_group" "levelup-securityelb" { 
+    vpc_id = aws_vpc.levelup_vpc.id
+    name = "levelup-securityelb"
+    
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+
+    }
+
+    tags = {
+      Name = "levelup-securityelb"
+    }
+}
+
+## security group for instances
+
+resource "aws_security_group" "levelup-security-instance" { 
+    vpc_id = aws_vpc.levelup_vpc.id
+    name = "levelup-security-instance"
+    
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+
+    }
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        security_groups = [aws_security_group.levelup-securityelb.id] 
+        ## - Instead of allowing requests from any IP address (cidr_blocks = ["0.0.0.0/0"]), 
+        ## it only allows traffic coming from the ELB security group (levelup-securityelb)
+    }
+
+    tags = {
+      Name = "levelup-security-instance"
+    }
+}
