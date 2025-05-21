481aae38a4ace2b7a3f8be8818695e9107275019
Harish-365 <bvpetro97@gmail.com>
2025-05-21 10:09:03 +0530

    add86

--- TerraformVariabledetailedexplanation/Terraform_modules/custom_vpc/Development_QA/main.tf
+++ TerraformVariabledetailedexplanation/Terraform_modules/custom_vpc/Development_QA/main.tf
@@ -1,7 +1,7 @@
 module "developmentqa-vpc" {
     source = "../../custom_vpc"
     vpcname = "dev-qa-01"
-    cidr = "10.0.1.0/24"
+    cidr = "10.0.2.0/24"
     enable_dns_support = "true"
     enable_dns_hostnames = "true"
     enable_ipv6 = "false"
