e229151c719415efd00796590cdcfcadb7595dfe
Harish-365 <bvpetro97@gmail.com>
2025-05-22 22:06:57 +0530

    add103

--- TerraformVariabledetailedexplanation/Terraform_Advance/conditional_statement/createinstance.tf
+++ TerraformVariabledetailedexplanation/Terraform_Advance/conditional_statement/createinstance.tf
@@ -6,7 +6,7 @@ provider "aws" {
 module "My-ec2-instance" {
     source = "github.com/terraform-aws-modules/terraform-aws-ec2-instance.git"
 
-    name = "my-cluster"
+    name_preffix = "my-cluster"
     instance_type = "t2.micro"
     ami = "ami-00045d6bafc77e3dc"
     subnet_id = "subnet-05c89a7f34226dece"
