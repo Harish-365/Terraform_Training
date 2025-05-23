24e31b89e4c5d9056e388a0541a2960d35c63aeb
Harish-365 <bvpetro97@gmail.com>
2025-05-23 18:54:34 +0530

    add121

--- TerraformVariabledetailedexplanation/Terraform_project_structure/Development/main.tf
+++ TerraformVariabledetailedexplanation/Terraform_project_structure/Development/main.tf
@@ -14,7 +14,7 @@ module "dev-instances" {
     source = "../modules/createinstance"
     ENVIRONMENT = var.ENVIRONMENT
     AWS_REGION = var.AWS_REGION
-    vpc_id = module.dev-vpc.My_vpc_id
+    vpc_id = module.dev-vpc.my_vpc_id
     PUBLIC_SUBNETS = module.dev-vpc.public_subnets
 }
 
