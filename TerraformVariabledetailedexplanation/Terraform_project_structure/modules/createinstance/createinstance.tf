7d8a8ff1aa3fc27300ebc885b8adc18f9c831492
Harish-365 <bvpetro97@gmail.com>
2025-05-23 18:55:57 +0530

    add123

--- TerraformVariabledetailedexplanation/Terraform_project_structure/modules/createinstance/createinstance.tf
+++ TerraformVariabledetailedexplanation/Terraform_project_structure/modules/createinstance/createinstance.tf
@@ -9,7 +9,7 @@ resource "aws_security_group" "allow-ssh" {
     vpc_id = var.vpc_id
 
     egress {
-        from_port = o
+        from_port = 0
         to_port = 0
         protocol = "-1"
         cidr_blocks = ["0.0.0.0/0"]
