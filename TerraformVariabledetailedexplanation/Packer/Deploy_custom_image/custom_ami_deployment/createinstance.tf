2444f122d880ab7210f4dc4572ff191ce0eeb7c0
Harish-365 <bvpetro97@gmail.com>
2025-05-25 10:01:27 +0530

    add143

--- TerraformVariabledetailedexplanation/Packer/Deploy_custom_image/custom_ami_deployment/createinstance.tf
+++ TerraformVariabledetailedexplanation/Packer/Deploy_custom_image/custom_ami_deployment/createinstance.tf
@@ -25,7 +25,7 @@ resource "aws_security_group" "allow-ssh" {
         from_port = 0
         to_port = 0
         protocol = "-1"
-        cidr_blocks = "0.0.0.0/0"
+        cidr_blocks = ["0.0.0.0/0"]
     
     }
 
@@ -33,7 +33,7 @@ resource "aws_security_group" "allow-ssh" {
         from_port = 22
         to_port = 22
         protocol = "tcp"
-        cidr_blocks = "0.0.0.0/0"
+        cidr_blocks = ["0.0.0.0/0"]
     }
     tags = {
         Name = "allow-ssh-${var.ENVIRONMENT}"
