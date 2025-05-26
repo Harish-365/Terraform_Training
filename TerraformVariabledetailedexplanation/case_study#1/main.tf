d4879629d074e5c800ea079ac457f42a0f7ff0d0
Harish-365 <bvpetro97@gmail.com>
2025-05-26 17:15:57 +0530

    add160

--- TerraformVariabledetailedexplanation/case_study#1/main.tf
+++ TerraformVariabledetailedexplanation/case_study#1/main.tf
@@ -1,15 +1,16 @@
-module "levelup-vpc" {
-    source      = "./module/vpc"
+# module "levelup-vpc" {
+#     source      = "./module/vpc"
 
-    ENVIRONMENT = var.ENVIRONMENT
-    AWS_REGION  = var.AWS_REGION
-}
+#     ENVIRONMENT = var.ENVIRONMENT
+#     AWS_REGION  = var.AWS_REGION
+# }
 
 module "levelup-webserver" {
     source      = "./webserver"
 
     ENVIRONMENT = var.ENVIRONMENT
     AWS_REGION  = var.AWS_REGION
+
 }
 ##Define provider
 
